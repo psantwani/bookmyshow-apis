03905d5f32b5948d2109317c5819bc2768d9a508
Piyush Santwani <psantwani@gmail.com>
2016-09-03 02:42:49 +0530

    showtimings

--- index.js
+++ index.js
@@ -48,6 +48,16 @@ app.get('/movies', apicache('1 hour'), function(req, res){
 	});
 });
 
+/**
+app.get('/movies/:filter', apicache('1 hour'), function(req, res){
+	var filter = '/' + req.params.filter;
+	movieListFiltered('', filter, function(data){
+		res.send(data);
+	});
+});
+**/
+
+
 app.get('/:city/movies', apicache('1 hour'), function(req, res){
 	var city = req.params.city.toLowerCase() + "/";
 	movieList(city, function(data){
@@ -66,6 +76,17 @@ app.get('/:city/movies', apicache('1 hour'), function(req, res){
 	});	
 });
 
+/**
+app.get('/:city/movies/:filter', apicache('1 hour'), function(req, res){
+	var city = req.params.city;
+	var filter = '/' + req.params.filter;
+	movieList(city, filter, function(data){
+		res.send(data);
+	});
+});
+**/
+
+
 app.get('/movies/:movie_name/:movie_code', function(req, res){
 	var movie_name = req.params.movie_name.toLowerCase();
 	var movie_code = req.params.movie_code.toUpperCase();
@@ -74,8 +95,43 @@ app.get('/movies/:movie_name/:movie_code', function(req, res){
 	});
 });
 
-function movieList(city, callback){
-	request('https://in.bookmyshow.com/' + city + 'movies', function (error, response, body) {
+app.get('/:city/buytickets/:movie_name/:movie_code/:date', function(req, res){
+	var city = req.params.city.toLowerCase();
+	var movie_name = req.params.movie_name.toLowerCase();
+	var movie_code = req.params.movie_code.toUpperCase();
+	var date = req.params.date;
+	showtimes(city, movie_name, movie_code, date, function(data){
+		res.send(data);
+	});
+});
+
+function showtimes(city, movie_name, movie_code, date, callback){
+	movie_name = movie_name.replace(/[^a-zA-Z ]/g, "");
+	movie_name = movie_name.replace(" ","-");
+	getCityCode(city, function(city_code){
+		request('https://in.bookmyshow.com/buytickets/' + movie_name + '-' + city + "/movie-" + city_code + "-" + movie_code + "-MT/" + date  , function (error, response, body) {
+		  	if (!error && response.statusCode == 200) {
+		  		var pattern = 'var aST_details  =   .*';
+		  		regExFunction(pattern, body, function(res){
+		  			if(res){	  				
+		  				var movieList = res.split("var aST_details  =   JSON.parse('")[1].replace(/'/g, "").slice(0,-2);		  				
+		  				return callback(movieList);
+		  			}
+		  			else{	  				
+		  				return callback("No data found");
+		  			}  			
+		  		});	  		
+		  	}
+		});
+	});
+}
+
+function getCityCode(city, callback){
+	return callback("mumbai");
+}
+
+function movieList(city, filter, callback){
+	request('https://in.bookmyshow.com/' + city + 'movies' + filter, function (error, response, body) {
 	  	if (!error && response.statusCode == 200) {
 	  		var pattern = 'var productClickArray = .*';
 	  		regExFunction(pattern, body, function(res){
